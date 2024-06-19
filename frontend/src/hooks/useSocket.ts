89c0f0f45aebe21d8d8c6c08ea0dae5d2f1224bc
Amber Li <al4580@columbia.edu>
2024-06-19 17:21:11 -0500

    change useSocket to vite instead of react

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 
-const WS_URL = "ws://localhost:8080";
+const backendWSUrl = import.meta.env.VITE_BACKEND_WS_URL;
+console.log(backendWSUrl);
 
 export const useSocket = () => {
     const [socket, setSocket] = useState<WebSocket | null>(null);
 
     useEffect(() => {
-        const ws = new WebSocket(WS_URL);
+        const ws = new WebSocket(backendWSUrl);
         ws.onopen = () => {
             setSocket(ws);
+            console.log("ws on open");
         }
         ws.onclose = () => {
             setSocket(null);
